f5e1b3ba2787cd422edfd1b88809420f593d26d8
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-05-26 06:10:30 +0530

    React Routing

---
+++ routing-r/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <button><a href="/about-us">About Us Page</a></button>
+    </div>
+  )
+}
+
+export default Home
