3026cd9cc9f3ad29ecfed412d9bec757b071e9d2
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-06-01 23:39:37 +0530

    Routing

--- routing-r/src/pages/Contact.jsx
+++ routing-r/src/pages/Contact.jsx
@@ -4,6 +4,7 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact Us Page</h1>
+      <a href="/">Home</a>
     </div>
   )
 }
--- routing-r/src/pages/Home.jsx
+++ routing-r/src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <button><a href="/about-us">About Us Page</a></button>
+      <button><a href="/contact-us">Contact Us Page</a></button>
     </div>
   )
 }
