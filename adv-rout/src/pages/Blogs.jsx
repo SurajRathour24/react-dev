94f79880c0f21988c6806c1e970027be37cc676a
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-06-04 23:49:41 +0530

    new

--- adv-rout/src/pages/Blogs.jsx
+++ adv-rout/src/pages/Blogs.jsx
@@ -4,6 +4,7 @@ const Blogs = () => {
   return (
     <div>
       <h1>Blog</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, corrupti quod possimus labore velit autem, quidem iste esse ipsa error beatae reiciendis perspiciatis? Alias ab mollitia iste, dolore suscipit exercitationem.</p>
     </div>
   )
 }
